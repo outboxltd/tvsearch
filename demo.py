468d9d15e7551421618a857d9ff6f88e487cd8a3
outboxltd <benatiya@gmail.com>
2019-07-20 17:16:14 +0300

    hello world :)

--- demo.py
+++ demo.py
@@ -5,14 +5,28 @@ from pprint import pprint
 with open('./data/143.json') as the_file:
     data = json.load(the_file)
 
+episodes = data['_embedded']['episodes']
+
 # print(data)
 
 # for i in data:
 #     pprint(i['url']);
 
 # for i in data['_embedded']['episodes']:
-#    print(i['url'])
-#
+#    print(i)
+
 # get images
-for i in data['_embedded']['episodes']:
-   print(i['image']['medium'])
+# for i in data['_embedded']['episodes']:
+#    print(i['image']['medium'])
+
+# get images
+# for i in data['_embedded']['episodes']:
+#    print(i['image']['medium'])
+
+# get images
+# for i in data['_embedded']['episodes']:
+#    print(i['name'])
+
+
+for i in episodes:
+   print(i['name'])
--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import os
-from bottle import (get, post, redirect, request, route, run, static_file,
-                    template)
+from bottle import (get, post, redirect, request, route, run, static_file,error,template,jinja2_view)
+from jinja2 import Template
+import json
 import utils
+
+# testing
+with open('./data/143.json') as the_file:
+    data = json.load(the_file)
+
+episodes = data['_embedded']['episodes']
+# testing
+
+
+
+
 
 # Static Routes
 
@@ -31,5 +43,12 @@ def Browse():
 def Search():
     sectionTemplate = "./templates/search.tpl"
     return template("./pages/search.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData = {})
+
+
+@route('/about')
+@jinja2_view('about.html', template_lookup=['pages'])
+def index2():
+    sectionTemplate = "./templates/browse.tpl"
+    return {'name': "shmulik" ,"tricks": ["קפיצה בחבל", "תפיסת חתולים", "האכלת כלבים אחרים"],'episodes': episodes }
 
 run(host='0.0.0.0', debug=True, reloader=True, port=os.environ.get('PORT', 5000))
